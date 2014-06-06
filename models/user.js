118e2857b09086eb02376fde35ecaa92ed7ab708
Albert <albert.vico@midokura.com>
2014-06-06 18:16:45 +0200

    added user.js

---
+++ models/user.js
@@ -0,0 +1,9 @@
+/* /models/user.js
+ * */
+App.User = DS.Model.extend({
+      name         : DS.attr(),
+  email        : DS.attr(),
+  bio          : DS.attr(),
+  avatarUrl    : DS.attr(),
+  creationDate : DS.attr()
+});
